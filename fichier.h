b53462122efdae19b04ce4ddef986ce1181888d5
suricate-larry <kamaartaj@gmail.com>
2025-04-08 17:35:45 +0200

    truc

--- fichier.h
+++ fichier.h
@@ -3,7 +3,13 @@
 #include <math.h>
 #include <string.h>
 #include <time.h>
+#include <SDL.h>
 #include "structs.h"
+
+Tetromino tetrominoConstructor(char tab[5][5]);
+int move_t(Tetromino *t,Vecteur v);
+void place_t(Tetromino *t,int tab[LINE][COL]);
+
 
 
 

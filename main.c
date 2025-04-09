2f4d758c35362cb90fda6ad44ee76cca4c554dd4
suricate-larry <kamaartaj@gmail.com>
2025-04-09 20:19:09 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,5 +6,31 @@
 
 int main(){
 	printf("Sah Dimitri\n");
+	App app;
+	int rendererFlags, windowFlags;
+
+    rendererFlags = SDL_RENDERER_ACCELERATED;
+
+	windowFlags = 0;
+	if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "[debug] %s", SDL_GetError());
+        return -1;
+	}
+	app.window = SDL_CreateWindow("Tetris", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, windowFlags);
+	if (!app.window)
+    {
+        printf("Failed to open %d x %d window: %s\n", SCREEN_WIDTH, SCREEN_HEIGHT, SDL_GetError());
+        exit(1);
+    }
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+	app.renderer = SDL_CreateRenderer(app.window, -1, rendererFlags);
+	if (!app.renderer)
+    {
+        printf("Failed to create renderer: %s\n", SDL_GetError());
+        exit(1);
+    }
+	SDL_DestroyWindow(app.window);     
+    SDL_Quit();
 	return 0;
 }

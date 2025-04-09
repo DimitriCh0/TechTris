177d14014eb5f2bef32240feefe31f3980023e05
suricate-larry <kamaartaj@gmail.com>
2025-04-09 20:17:15 +0200

    Update input.c

--- input.c
+++ input.c
@@ -1,1 +1,20 @@
 #include "fichier.h"
+
+
+void doInput(void)
+{
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+            case SDL_QUIT:
+                exit(0);
+                break;
+
+            default:
+                break;
+        }
+    }
+}

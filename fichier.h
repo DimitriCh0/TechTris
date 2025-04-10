604abaac2b2dc9e766d7a76971121746163fb50d
suricate-larry <kamaartaj@gmail.com>
2025-04-10 20:56:13 +0200

    truc

--- fichier.h
+++ fichier.h
@@ -16,8 +16,9 @@
 #define SCREEN_HEIGHT  720
 
 Tetromino tetrominoConstructor(char tab[5][5]);
-int move_t(Tetromino *t,Vecteur v);
-void place_t(Tetromino *t,int tab[LINE][COL]);
+int move_t(Tetromino *t,Vecteur v)
+void place_t(Tetromino *t,int tab[LINE][COL],Vecteur v);
+int stillAlive(Tetromino *t, Vecteur v, int tab[LINE][COL]);
 int key_input();
 Vecteur keyToVect(int r);
 void display(char tab[LINE][COL]);

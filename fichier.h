5843123b51ac8560ef3880ca6e51de5a8a46f89d
suricate-larry <kamaartaj@gmail.com>
2025-04-11 16:52:31 +0200

    Update fichier.h

--- fichier.h
+++ fichier.h
@@ -6,14 +6,11 @@
 #include <math.h>
 #include <string.h>
 #include <time.h>
-#include <SDL2/SDL.h>
 #include <unistd.h>
 
 
 #define COL 10
 #define LINE 10
-#define SCREEN_WIDTH   1280
-#define SCREEN_HEIGHT  720
 
 Tetromino tetrominoConstructor(char tab[5][5]);
 int move_t(Tetromino *t,Vecteur v);

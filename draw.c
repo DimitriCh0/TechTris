f5ba7dc17907b696866f95d0cc528b4940a481a3
DimitriCh0 <dimitrichau@gmail.com>
2025-05-11 17:16:19 +0200

    V3

--- draw.c
+++ draw.c
@@ -8,7 +8,7 @@ void display_tetromino(Tetromino *t, int line){
     	int bloc_placed = 0;
     	int dx = 2 - t->blocs[0][0]; //Distance x entre le centre du tetromino et la case (2,2), qui est le centre d'un tableau 5*5
     	int dy = 2 - t->blocs[0][1]; //Distance y...
-    	printf("▦      ▐ ");
+    	printf("🧱      🧱 ");
     	for (int i = 0; i<DIM; i++){
         	for (int j=0; j<t->nb_blocs;j++){
             		if(t->blocs[j][0]+dx == line && t->blocs[j][1]+dy == i){
@@ -23,7 +23,7 @@ void display_tetromino(Tetromino *t, int line){
         	}
         	bloc_placed = 0;
 	}
-    	printf("▐\n");
+    	printf("🧱\n");
 }
 
 //Affiche dans le terminal la grille avec les tetrominos, le pseudo, le score et la pièce suivante
@@ -33,39 +33,39 @@ void display(char grille[LINE][COL][UTF], Joueur* J, Tetromino *t, int s){
         	exit(71);
     	}
     	for (int i = 0; i<LINE; i++){
-        	printf("      ▦");
+        	printf("      🧱");
         	for (int j = 0; j<COL; j++){
             		printf("%s",grille[i][j]);
         	}
         
         	if (i == 2){
-            		printf("▦      Pseudo : %s\n",J->pseudo);
+            		printf("🧱      Pseudo : %s\n",J->pseudo);
         	}
 		else if(i==3){
-            		printf("▦      Difficulté : %d\n",J->difficulte);
+            		printf("🧱      Difficulté : %d\n",J->difficulte);
         	}
 		else if(i==4){
             		if (J->score > 999999){
             			J->score = 999999;
             		}
-            		printf("▦      Score : %d\n",J->score);
+            		printf("🧱      Score : %d\n",J->score);
         	}
 		else if (LINE >12 && i>5 && i<=12 && s){
             		if (i==6){
-                		printf("▦       ************\n");
+                		printf("🧱       🧱🧱🧱🧱🧱🧱🧱\n");
             		}
 			else if(i>6 && i<=11){
                 		display_tetromino(t,i-7);
             		}
 			else if(i==12){
-                		printf("▦       ************\n");
+                		printf("🧱       🧱🧱🧱🧱🧱🧱🧱\n");
             		}
         	}
 		else {
-            		printf("▦\n");
+            		printf("🧱\n");
         	}
     }
-    printf("      **********************\n");
+    printf("      🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱🧱\n");
     
 }
 //Affiche dans le terminal un double tableau d'entiers
